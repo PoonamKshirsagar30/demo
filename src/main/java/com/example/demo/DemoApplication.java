03138832f8a6f6bf4252e129db2473cf36239c06
poonam kshirsgar <poonamkshirsgaar3012@gmail.com>
2026-02-06 16:17:23 +0530

    second statement added

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
         System.out.println("Hello Poonam");
+        System.out.println("one more statement added !");
     }
 
 }
